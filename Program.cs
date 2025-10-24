090f38d4443ee1c910a8311db680e475758a4b15
Hls5235 <hunter.simpson@comcast.net>
2025-10-24 08:55:33 -0400

    Updated .NET version

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MapControllers();
 
-// Get port from environment or default to 8080
+// Read the PORT environment variable injected by Cloud Run, default to 8080
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
